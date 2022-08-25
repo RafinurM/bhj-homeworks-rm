08d26137a0c28142176268f6ffd84c90db79ae72
RafinurM <zadorinka16@mail.ru>
2022-08-26 01:58:09 +0700

    Update task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,12 +1,12 @@
-let timerCount = document.getElementById("timer")
+let timerCount = document.getElementById("timer");
 
 const counter = function () {
-  if (timerCount.textContent === '0') {
+  if (timerCount.textContent === "0") {
     alert("Вы победили в конкурсе!");
-    timerCount.textContent = 15;
+    clearInterval(timer);
   } else {
     timerCount.textContent -= 1;
   }
 };
 
-setInterval(counter, 1000); 
+let timer = setInterval(counter, 1000);
