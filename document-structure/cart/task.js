bb7745eb1e8083b0d1c59cfeb3ea65c0c45f3aa7
RafinurM <zadorinka16@mail.ru>
2022-08-29 03:26:57 +0700

    Update task.js

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,35 +1,47 @@
-let incBtn = document.querySelectorAll('div.product__quantity-control_inc');
-let decBtn = document.querySelectorAll('div.product__quantity-control_dec');
-let addBtn = document.querySelectorAll('div.product__add');
-let cart = document.querySelector('div.cart__products');
+let incBtn = document.querySelectorAll("div.product__quantity-control_inc");
+let decBtn = document.querySelectorAll("div.product__quantity-control_dec");
+let addBtn = document.querySelectorAll("div.product__add");
+let cart = document.querySelector("div.cart__products");
+let products = document.querySelectorAll("div.product");
 
-
-let incBtnArr = Array.from(incBtn)
+let incBtnArr = Array.from(incBtn);
 incBtnArr.forEach((item) => {
-    item.addEventListener('click', () => {
-        item.previousElementSibling.innerHTML++;
-    })
+  item.addEventListener("click", () => {
+    item.previousElementSibling.innerHTML++;
+  });
 });
 
 let decBtnArr = Array.from(decBtn);
 decBtnArr.forEach((item) => {
-    item.addEventListener('click', () => {
-        if (item.nextElementSibling.textContent <= 1) return;
-        item.nextElementSibling.innerHTML--;  
-    })
+  item.addEventListener("click", () => {
+    if (item.nextElementSibling.textContent <= 1) {
+      return;
+    }
+    item.nextElementSibling.innerHTML--;
+  });
 });
 
 let addBtnArr = Array.from(addBtn);
 addBtnArr.forEach((item) => {
-    item.addEventListener('click', () => {
-        let dataId = item.closest('.product').getAttribute('data-id');
-        let imageSrc = item.closest('.product__controls').previousElementSibling.getAttribute('src')
-        let count = item.previousElementSibling.firstElementChild.nextElementSibling.innerHTML;
-        cart.insertAdjacentHTML('beforeEnd', 
+  item.addEventListener("click", () => {
+    let dataId = item.closest(".product").getAttribute("data-id");
+    let imageSrc = item.closest(".product__controls").previousElementSibling.getAttribute("src");
+    let count = +item.previousElementSibling.firstElementChild.nextElementSibling.innerHTML;
+    let cartProducts = Array.from(document.querySelectorAll(".cart__product"));
+    let findedItem = cartProducts.find(
+      (item) => item.getAttribute("data-id") === dataId
+    );
+    if (findedItem) {
+        let findedItemCount = +findedItem.lastElementChild.innerHTML
+        findedItem.lastElementChild.innerHTML = `${count + findedItemCount}`
+    } else {
+      cart.insertAdjacentHTML(
+        "beforeEnd",
         `<div class="cart__product" data-id="${dataId}">
-        <img class="cart__product-image" src="${imageSrc}">
-        <div class="cart__product-count">${count}</div>
-    </div>` )
-    })
+              <img class="cart__product-image" src="${imageSrc}">
+              <div class="cart__product-count">${count}</div>
+          </div>`
+      );
+    }
+  });
 });
-
