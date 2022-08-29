2d5cdd7e6d27b604d3c39a7ab87bcc88f25694af
RafinurM <zadorinka16@mail.ru>
2022-08-29 23:22:19 +0700

    Update task.js -add remove tips

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -7,9 +7,17 @@ tip.classList.add("tooltip_active");
 helpTextArr.forEach((item) => {
   item.addEventListener("click", (e) => {
     e.preventDefault();
+    let tipActive = document.querySelector('.tooltip_active')
+    console.log(tipActive)
+    if (tipActive) {
+      tipActive.remove();    
+      return  
+    }
     let tipTitle = item.getAttribute("title"); 
     tip.innerText = tipTitle; 
+    
     item.insertAdjacentElement("afterEnd", tip);
+    
     let tipPosition = item.getBoundingClientRect();
     tip.style.left = `${tipPosition.left}px`
   });
